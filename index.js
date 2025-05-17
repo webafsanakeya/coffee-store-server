760c7726d60ef51c955b48aa64b916acfc7a1c24
webafsanakeya <webafsakeya@gmail.com>
2025-05-18 01:43:42 +0600

    addes users CRUD apis

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
     await client.connect();
 
     const coffeesCollection = client.db('coffeeDB').collection('coffees');
+    const usersCollection = client.db('coffeesDB').collection('users');
 
     app.get('/coffees', async(req, res)=>{
         // const cursor = coffeesCollection.find();
@@ -70,6 +71,47 @@ async function run() {
         const result = await coffeesCollection.deleteOne(query);
         res.send(result); 
 
+    })
+
+    // User related APIs
+
+    app.get('/users', async(req, res)=>{
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+
+
+
+
+
+
+    app.post('/users', async(req, res)=>{
+      const userProfile = req.body;
+      console.log(userProfile);
+      const result = await usersCollection.insertOne(userProfile);
+      res.send(result);
+    })
+
+
+    app.patch('/users', async(req, res)=>{
+      const {email, lastSignInTime} = req.body;
+      const filter = {email: email}
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: lastSignInTime
+        }
+      }
+
+      const result = await usersCollection.updateOne(filter,updatedDoc)
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
     })
 
 
